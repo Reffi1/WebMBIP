0696de17b46963ac478219f98ddd0c72591ec187
Reffi1 <reffireskian321@gmail.com>
2026-02-10 16:21:12 +0700

    Perbaikan slide

--- src/app/components/Home/Companies/index.tsx
+++ src/app/components/Home/Companies/index.tsx
@@ -18,7 +18,9 @@ const Companies = () => {
     autoplay: true,
     speed: 1000,
     autoplaySpeed: 3000,
-    cssEase: 'linear',
+    cssEase: 'ease-in-out',
+    pauseOnHover: true,
+    swipeToSlide: true,
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 4, slidesToScroll: 1 } },
       { breakpoint: 700, settings: { slidesToShow: 3, slidesToScroll: 1 } },
